21762e15c042caddf3b789bc099d888cb3179a15
Alex Tcoi <alexvtcoi@gmail.com>
2021-05-04 09:55:12 +0200

    добавил обработку ошибок по некорректным роутам и повторная проверка регулярки на аватаре

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cardsRouter = require("./routes/cards");
 const usersRouter = require("./routes/users");
 const { login, createUser } = require("./controllers/users");
 const auth = require("./middlewares/auth");
+const NotFoundError = require("./errors/not-found-error");
 
 const {
   PORT = 3000,
@@ -21,7 +22,8 @@ app.post('/signin', celebrate({
     email: Joi.string().required().email(),
     password: Joi.string().min(8).required(),
   })
-}), login);
+}), login);// логинимся на сервисе
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
@@ -30,14 +32,19 @@ app.post('/signup', celebrate({
     email: Joi.string().required().email(),
     password: Joi.string().min(8).required(),
   })
-}), createUser);
+}), createUser);// регистрация нового пользователя
 
-app.use(auth);
+app.use("/:wrongRoute", (req, res, next) => {
+  const err = new NotFoundError('Указан неправильный путь или метод');
+  next(err);
+});// проверка корректности роута
 
-app.use("/users", usersRouter);
-app.use("/cards", cardsRouter);
+app.use(auth);// вход по токену
 
-app.use(errors());
+app.use("/users", usersRouter);// методы для пользователей
+app.use("/cards", cardsRouter);// методы для карточек
+
+app.use(errors());// проверка данных для сервера
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
@@ -50,7 +57,7 @@ app.use((err, req, res, next) => {
         ? 'На сервере произошла ошибка'
         : message
     });
-});
+});// централизованная обработка ошибок
 
 async function launch() {
   await mongoose.connect(MONGO_URL, {
@@ -61,6 +68,6 @@ async function launch() {
   await app.listen(PORT);
 }
 
-launch();
+launch();// слушаем сервер
 
 // https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg
